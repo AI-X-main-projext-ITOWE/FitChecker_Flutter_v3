124f1f8ab647d8766b7a58e0831a55ff6cafa506
hwang jeong han <hjeonghan0@gmail.com>
2024-12-16 14:04:38 +0900

    feat : "메인수정"

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,11 @@
+import 'package:fitchecker/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-FlutterLocalNotificationsPlugin();
-
-// 백그라운드 메시지 핸들러
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  print("백그라운드 메시지 수신: ${message.messageId}");
-  showLocalNotification(message); // 로컬 알림 표시
-}
-
-// 로컬 알림 표시 함수
-void showLocalNotification(RemoteMessage message) async {
-  const AndroidNotificationDetails androidPlatformChannelSpecifics =
-  AndroidNotificationDetails(
-    'your_channel_id', // 알림 채널 ID
-    'your_channel_name', // 알림 채널 이름
-    importance: Importance.max,
-    priority: Priority.high,
-  );
-
-  const NotificationDetails platformChannelSpecifics =
-  NotificationDetails(android: androidPlatformChannelSpecifics);
-
-  await flutterLocalNotificationsPlugin.show(
-    0, // Notification ID
-    message.notification?.title ?? '알림', // 제목
-    message.notification?.body ?? '내용',  // 내용
-    platformChannelSpecifics,
-  );
-}
+import 'package:provider/provider.dart';
+import 'components/notification_helper.dart';  // Provider import
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -49,21 +20,18 @@ void main() async {
   // Firebase 초기화
   await Firebase.initializeApp();
 
-  // FCM 백그라운드 핸들러 등록
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  // Flutter Local Notifications 초기화
-  const AndroidInitializationSettings initializationSettingsAndroid =
-  AndroidInitializationSettings('@mipmap/ic_launcher');
-  const InitializationSettings initializationSettings =
-  InitializationSettings(android: initializationSettingsAndroid);
-  await flutterLocalNotificationsPlugin.initialize(initializationSettings);
-
-  // FCM 토큰 가져오기
+  // FCM 설정
   String? fcmToken = await FirebaseMessaging.instance.getToken();
   print("FCM Token: $fcmToken");
 
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<NotificationHelper>(create: (_) => NotificationHelper()),  // NotificationHelper Provider 추가
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -76,7 +44,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
-
     );
   }
 }
