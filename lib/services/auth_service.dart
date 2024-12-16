d5808a77a3e87ad392b726a5a1e4e279a4932ed5
hwang jeong han <hjeonghan0@gmail.com>
2024-12-16 17:14:00 +0900

    feat : "로그인 수정 .."

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,15 +1,17 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; // Firestore 추가
-import 'package:fitchecker/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart' as kakao_user;
 
 class AuthService {
-  static final FirebaseAuth _auth = FirebaseAuth.instance;
+  static final firebase_auth.FirebaseAuth _auth = firebase_auth.FirebaseAuth.instance;
   static final GoogleSignIn _googleSignIn = GoogleSignIn();
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // 구글 로그인
-  static Future<User?> signInWithGoogle() async {
+  static Future<firebase_auth.User?> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       if (googleUser == null) {
@@ -17,54 +19,91 @@ class AuthService {
       }
 
       final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-      final AuthCredential credential = GoogleAuthProvider.credential(
+      final firebase_auth.AuthCredential credential = firebase_auth.GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+      final firebase_auth.UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      await _saveUserToFirestore(userCredential.user!);
       return userCredential.user;
     } catch (e) {
       throw Exception('Google Sign-In failed: $e');
     }
   }
 
-
-  // Firestore에 사용자 정보 저장
-  static Future<void> _saveUserToFirestore(User user) async {
+// 카카오 로그인 함수
+  static Future<firebase_auth.User?> signInWithKakao(BuildContext context) async {
     try {
-      // Firestore에 사용자 정보가 이미 존재하는지 확인
+      bool isInstalled = await kakao_user.isKakaoTalkInstalled();
+      kakao_user.OAuthToken token;
+
+      if (isInstalled) {
+        token = await kakao_user.UserApi.instance.loginWithKakaoTalk();
+      } else {
+        token = await kakao_user.UserApi.instance.loginWithKakaoAccount();
+      }
+
+      print('카카오 로그인 성공: ${token.accessToken}');
+
+      // Firebase 인증 자격 증명 생성
+      final firebase_auth.OAuthCredential credential = firebase_auth.OAuthProvider("kakao.com").credential(
+        accessToken: token.accessToken,
+      );
+
+      // Firebase로 로그인
+      final firebase_auth.UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      // Firestore에 사용자 정보 저장 (필요한 경우)
+      await _saveKakaoUserToFirestore(userCredential.user!);
+
+
+      return userCredential.user;
+    } catch (e) {
+      print('카카오 로그인 실패: $e');
+      return null;
+    }
+  }
+
+
+  // Firestore에 구글 사용자 정보 저장
+  static Future<void> _saveUserToFirestore(firebase_auth.User user) async {
+    try {
       DocumentReference userDocRef = _firestore.collection('users').doc(user.uid);
 
-      // Firestore에 사용자 정보 저장
       await userDocRef.set({
         'uid': user.uid,
         'email': user.email,
         'displayName': user.displayName,
         'photoURL': user.photoURL,
         'lastLogin': FieldValue.serverTimestamp(),
+        'provider': 'google',
       }, SetOptions(merge: true));
 
-      print('사용자 정보 Firestore에 저장됨');
+      print('구글 사용자 정보 Firestore에 저장됨');
     } catch (e) {
       print('Firestore 저장 실패: $e');
     }
   }
 
-  // 로그아웃
-  static Future<void> signOut() async {
-    await _googleSignIn.signOut();
-    await _auth.signOut();
-  }
+  // Firestore에 카카오 사용자 정보 저장
+  static Future<void> _saveKakaoUserToFirestore(firebase_auth.User user) async {
+    try {
+      DocumentReference userDocRef = _firestore.collection('users').doc(user.uid);
 
-  // 현재 로그인한 사용자 정보 가져오기
-  static UserModel? getCurrentUser() {
-    final user = _auth.currentUser;
-    if (user != null) {
-      return UserModel.fromFirebaseUser(user);
+      await userDocRef.set({
+        'uid': user.uid,
+        'email': user.email,
+        'displayName': user.displayName,
+        'photoURL': user.photoURL,
+        'lastLogin': FieldValue.serverTimestamp(),
+        'provider': 'kakao',
+      }, SetOptions(merge: true));
+
+      print('카카오 사용자 정보 Firestore에 저장됨');
+    } catch (e) {
+      print('Firestore 저장 실패: $e');
     }
-    return null;
   }
-
-  static saveUserToFirestore(User user) {}
 }
